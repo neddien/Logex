70eff13927a60e41954a198043c7e434886960ec
stigranyan <serob.tigranyan@taurotech.com>
2026-04-22 10:51:43 +0000

    Integrated Conan as package manager. Moved {fmt} from a git submodule to a Conan library. Updated conanfile to export Logex as a Conan package.

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,33 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.scm import Git
 
-class MyLibConan(ConanFile):
+
+class LogexConan(ConanFile):
     name = "logex"
     version = "1.0.0"
+    url = "https://github.com/stigranyan/Logex"
 
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
 
-    exports_sources = "Logger/CMakeLists.txt", "Logger/src/*", "Logger/include/*"
     generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("fmt/12.1.0")
 
     def layout(self):
         cmake_layout(self)
 
+    def source(self):
+        git = Git(self)
+        git.clone(url=self.url, target=".")
+        git.checkout(f"v{self.version}")
+
     def build(self):
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(variables={"LGX_BUILD_TESTBED": "OFF"})
         cmake.build()
 
     def package(self):
@@ -25,6 +35,6 @@ class MyLibConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.set_property("cmake_target_name", "logex::Logex")
-        self.cpp_info.libs = ["Logex"]
-
+        self.cpp_info.set_property("cmake_target_name", "logex::logex-static")
+        self.cpp_info.libs = ["logex-static"]
+        self.cpp_info.includedirs = ["include/logex"]
